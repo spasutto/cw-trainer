7859907d27bd00a4e293a76faa485b145bce3577
Sylvain Pasutto <sylvainpasutto@gmail.com>
2024-11-04 11:44:03 +0100

    init render to file

--- morseplayer.js
+++ morseplayer.js
@@ -362,8 +362,12 @@ class CWPlayer {
     return Math.random() * (max - min) + min;
   }
 
-  initAudio() {
-    this.context = new (window.AudioContext || window.webkitAudioContext)();
+  initAudio(offline = false) {
+    if (!offline) {
+      this.context = new (window.AudioContext || window.webkitAudioContext)();
+    } else {
+      this.context = new OfflineAudioContext(2, 44100 * (this.totaltime+0.5), 44100);
+    }
     this.osc = this.context.createOscillator();
     this.gain = this.context.createGain();
     this.master = this.context.createGain();
@@ -568,6 +572,119 @@ class CWPlayer {
     });
     this.itime.push(d);
     return d;
+  }
+  async renderToFile() {
+    try {
+      this.initAudio(true);
+      this.playing = true;
+      this.schedule();
+      this.playing = false;
+      let audioBuffer = await this.context.startRendering();
+      console.log(audioBuffer);
+      
+      const [left, right] =  [audioBuffer.getChannelData(0), audioBuffer.getChannelData(1)]
+      
+      // interleaved
+      const interleaved = new Float32Array(left.length + right.length)
+      for (let src=0, dst=0; src < left.length; src++, dst+=2) {
+        interleaved[dst] =   left[src]
+        interleaved[dst+1] = right[src]
+      }
+      
+      // get WAV file bytes and audio params of your audio source
+      const wavBytes = WAV.getBytes(interleaved.buffer, {
+        isFloat: true,       // floating point or 16-bit integer
+        numChannels: 2,
+        sampleRate: 48000,
+      })
+      const wav = new Blob([wavBytes], { type: 'audio/wav' });
+
+      //document.querySelector("audio").src = URL.createObjectURL(blob);
+      //return;
+      let url = URL.createObjectURL(wav);
+      console.log(url);
+      let a = document.createElement('a');
+      a.href = url;
+      a.download = "file.wav";
+      document.body.appendChild(a);
+      a.click();    
+      a.remove();
+    } catch (e) {
+      alert('unable to save WAV file');
+      console.error(e);
+    } finally {
+      this.initAudio();
+    }
+  }
+}
+
+// https://stackoverflow.com/a/62173861
+class WAV {
+  // Returns Uint8Array of WAV bytes
+  static getBytes(buffer, options) {
+    const type = options.isFloat ? Float32Array : Uint16Array
+    const numFrames = buffer.byteLength / type.BYTES_PER_ELEMENT
+  
+    const headerBytes = WAV.getHeader(Object.assign({}, options, { numFrames }))
+    const wavBytes = new Uint8Array(headerBytes.length + buffer.byteLength);
+  
+    // prepend header, then add pcmBytes
+    wavBytes.set(headerBytes, 0)
+    wavBytes.set(new Uint8Array(buffer), headerBytes.length)
+  
+    return wavBytes
+  }
+  
+  // adapted from https://gist.github.com/also/900023
+  // returns Uint8Array of WAV header bytes
+  static getHeader(options) {
+    const numFrames =      options.numFrames
+    const numChannels =    options.numChannels || 2
+    const sampleRate =     options.sampleRate || 44100
+    const bytesPerSample = options.isFloat? 4 : 2
+    const format =         options.isFloat? 3 : 1
+  
+    const blockAlign = numChannels * bytesPerSample
+    const byteRate = sampleRate * blockAlign
+    const dataSize = numFrames * blockAlign
+  
+    const buffer = new ArrayBuffer(44)
+    const dv = new DataView(buffer)
+  
+    let p = 0
+  
+    function writeString(s) {
+      for (let i = 0; i < s.length; i++) {
+        dv.setUint8(p + i, s.charCodeAt(i))
+      }
+      p += s.length
+    }
+  
+    function writeUint32(d) {
+      dv.setUint32(p, d, true)
+      p += 4
+    }
+  
+    function writeUint16(d) {
+      dv.setUint16(p, d, true)
+      p += 2
+    }
+  
+    writeString('RIFF')              // ChunkID
+    writeUint32(dataSize + 36)       // ChunkSize
+    writeString('WAVE')              // Format
+    writeString('fmt ')              // Subchunk1ID
+    writeUint32(16)                  // Subchunk1Size
+    writeUint16(format)              // AudioFormat https://i.sstatic.net/BuSmb.png
+    writeUint16(numChannels)         // NumChannels
+    writeUint32(sampleRate)          // SampleRate
+    writeUint32(byteRate)            // ByteRate
+    writeUint16(blockAlign)          // BlockAlign
+    writeUint16(bytesPerSample * 8)  // BitsPerSample
+    writeString('data')              // Subchunk2ID
+    writeUint32(dataSize)            // Subchunk2Size
+  
+    return new Uint8Array(buffer)
   }
 }
 
