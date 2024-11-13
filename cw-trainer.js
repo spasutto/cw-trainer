e1905aa77a69c43f9045a4e99a622085a1b6da7f
Sylvain Pasutto <sylvainpasutto@gmail.com>
2024-11-13 08:19:09 +0100

    oubli du 'O' dans les lettres

--- cw-trainer.js
+++ cw-trainer.js
@@ -18,7 +18,7 @@ var cw_options = {
   keyqual: 1
 };
 const KOCHCARS = ['K', 'M', 'U', 'R', 'E', 'S', 'N', 'A', 'P', 'T', 'L', 'W', 'I', '.', 'J', 'Z', '=', 'F', 'O', 'Y', ',', 'V', 'G', '5', '/', 'Q', '9', '2', 'H', '3', '8', 'B', '?', '4', '7', 'C', '1', 'D', '6', '0', 'X'];
-const ALPHA = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+const ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const NUMBERS = "0123456789";
 const SYMBOLS = "/+=.,\"$'()[]-:;@_!?¶";
 var QSOs = [
